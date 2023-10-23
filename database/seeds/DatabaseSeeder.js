d6d900603dd4cdf2e756b42295826010cd239608
Luca P <lucaexequiel@gmail.com>
2023-10-23 17:41:23 -0300

    .

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -1,15 +1,5 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| DatabaseSeeder
-|--------------------------------------------------------------------------
-|
-| Make use of the Factory instance to seed database with dummy data or
-| make use of Lucid models directly.
-|
-*/
-
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory')
 const DirectorioSeeder = use('./DirectorioSeeder')
--- database/seeds/DirectorioSeeder.js
+++ database/seeds/DirectorioSeeder.js
@@ -1,15 +1,5 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| DirectorioSeeder
-|--------------------------------------------------------------------------
-|
-| Make use of the Factory instance to seed database with dummy data or
-| make use of Lucid models directly.
-|
-*/
-
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory')
 const Database = use('Database')
